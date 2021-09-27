9d7a2437859f3f705f8602363dea13ec13705f74
Nishabhakar <69414363+Nishabhakar@users.noreply.github.com>
2021-09-27 13:27:47 +0530

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,4 +1,5 @@
 #bubble sort function
+
 def bubbleSort(arr,n):
     if(n>0):
         for i in range(0,n):
@@ -8,14 +9,22 @@ def bubbleSort(arr,n):
                 arr[i + 1] = temp
         bubbleSort(arr, n - 1)
 
-#main fuctin
+#main functin
 arr=[]
+
+# to get the size from user
 n = int(input("Enter the size of ur array: "))
+
 print("Enter the Element of  array:")
+
 for i in range(0,n):
+    #to generate random numbers in array
     num = int(input())
     arr.append(num)
+    
 bubbleSort(arr, n - 1) #bubblesorrt function called!
+
 print("After Sorting Array Elements are:")
+
 for i in range(0,n):
     print(arr[i],end=" ")
